bb13bc4e5cba8385bc78ffedbefe976b8f01d1a8
Afk <m.abdullahh.1385@gmail.com>
2024-02-05 17:49:22 +0500

    Start Project

---
+++ app/src/main/java/com/m_abdullah/i21_1385/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.m_abdullah.i21_1385
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
