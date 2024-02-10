1d91a1d8e2a1797d9ef17186ff756ee881eb199f
Afk <m.abdullahh.1385@gmail.com>
2024-02-11 00:19:50 +0500

    Camera Pages

--- app/src/main/java/com/m_abdullah/i21_1385/Photo_Camera_Activity.kt
+++ app/src/main/java/com/m_abdullah/i21_1385/Photo_Camera_Activity.kt
@@ -3,9 +3,9 @@ package com.m_abdullah.i21_1385
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Add_Mentor : AppCompatActivity() {
+class Photo_Camera_Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.add_mentor_page)
+        setContentView(R.layout.photo_camera_page)
     }
 }
