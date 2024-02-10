9a07e719c4977993b7fbe4abac8b9a18bac0f0de
Afk <m.abdullahh.1385@gmail.com>
2024-02-10 23:25:20 +0500

    Chat Page

---
+++ app/src/main/java/com/m_abdullah/i21_1385/Chat_Text_Activity.kt
@@ -0,0 +1,11 @@
+package com.m_abdullah.i21_1385
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Chat_Text_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.text_chat_page)
+    }
+}
