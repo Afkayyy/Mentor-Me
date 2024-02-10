dd8cb14d0973fe55197f7d7fb21404f430a7644b
Afk <m.abdullahh.1385@gmail.com>
2024-02-10 23:46:48 +0500

    Community_Chat_Page

---
+++ app/src/main/java/com/m_abdullah/i21_1385/Community_Chat_Activity.kt
@@ -0,0 +1,11 @@
+package com.m_abdullah.i21_1385
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Community_Chat_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.community_chat_page)
+    }
+}
